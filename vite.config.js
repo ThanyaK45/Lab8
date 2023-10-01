4c5d3283a543e57e46fb213cdb28dc16765ea6a7
Thanyanon <thanyatle24@hotmail.com>
2023-10-01 23:07:25 +0700

    icon

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+
 
 
 // https://vitejs.dev/config/
