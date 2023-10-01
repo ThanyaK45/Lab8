d44e7728de2983a1fdcd13199753886cc9297e9d
Thanyanon <thanyatle24@hotmail.com>
2023-10-01 23:00:59 +0700

    icon

--- src/views/BlogHeader.vue
+++ src/views/BlogHeader.vue
@@ -72,9 +72,7 @@ import { RouterLink } from 'vue-router'
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <div class="col-4 pt-1">
-            <RouterLink class="link-secondary" to="/">
-              <img src="src\assets\icon.png" alt="Icon" width="50">
-            </RouterLink>
+
         </div>
         </div>
         <ul class="nav nav-pills">
