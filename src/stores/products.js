33be56b4d0134317890363ace9754486e74eef87
Thanyanon <thanyatle24@hotmail.com>
2023-10-08 20:50:28 +0700

    finish1

--- src/stores/products.js
+++ src/stores/products.js
@@ -10,7 +10,7 @@ export const useProductStore = defineStore({
         Name : 'ASRV เสื้อกันหนาวมีฮู้ด Unisex Top Coat',
         Price : '฿273.00',
         PriceCal : 273.00, 
-        img : "public/img/ASRV.png"},
+        img : "../img/ASRV.png"},
       { id : 2,
         Name : 'FARUILINE STORE กางเกงคาร์โก้ลําลอง',
         Price : '฿363.00',
